f22cd5d4bbd007585e6b254a06ea8301db95fe21
Amanda Hinchey <akhinchey@gmail.com>
2016-10-23 23:21:21 -0400

    switch repos, git workflow error

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  skip_before_filter  :verify_authenticity_token
+  # http_basic_authenticate_with name: "Amanda", password: "password", only: :destroy
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+end
